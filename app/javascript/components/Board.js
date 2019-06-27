9e740a38bdf582da3727d5bfbfc3013263fadff3
Daniel Porter <dp@danielporter.ca>
2019-06-27 22:19:31 +0200

    Scaffold proble, board, rack, tile.

---
+++ app/javascript/components/Board.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class Board extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Board;
---
+++ app/javascript/components/Problem.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Board from "./Board";
+import Rack from "./Rack";
+
+class Problem extends React.Component {
+  render () {
+    return (
+      <div>This is a problem.</div>
+    );
+  }
+}
+
+export default Problem;
