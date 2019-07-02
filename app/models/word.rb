a25f24ca3aa46faeea290e00218d38f0641ec281
Daniel Porter <dp@danielporter.ca>
2019-07-02 12:26:04 +0200

    Commiting final WordList initializer.

--- app/models/word.rb
+++ app/models/word.rb
@@ -11,19 +11,4 @@
 
 class Word < ApplicationRecord
   belongs_to :alphagram
-
-  def self.initialize_from_file(file_path)
-    File.readlines(file_path).each do |word_string|
-      chomped_string = word_string.chomp
-      word = find_or_create_by(text: chomped_string)
-      sorted_letters  = word.text.split('').sort.join
-      anagram = Anagram.find_or_create_by(sorted_letters: sorted_letters)
-      word.anagram = anagram
-      word.save
-    end
-  end
-
-  def anagrams
-    anagram.words
-  end
 end
